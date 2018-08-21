a05da0c0c31d322a9b36352cf0c0ff099955bd7d
Ceasar Barbosa <cbarbosa@solstice.com>
2018-08-21 10:48:48 -0500

    add persistence layer

--- Bike-Registry/src/main/java/com/globomatics/bike/controllers/BikesController.java
+++ Bike-Registry/src/main/java/com/globomatics/bike/controllers/BikesController.java
@@ -1,34 +1,41 @@
 package com.globomatics.bike.controllers;
 
 import com.globomatics.bike.models.Bike;
+import com.globomatics.bike.repositories.BikeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/bikes")
 public class BikesController {
 
+    private BikeRepository bikeRepository;
+
+    @Autowired
+    public BikesController(BikeRepository bikeRepository) {
+        this.bikeRepository = bikeRepository;
+    }
+
     // List Bikes
     @GetMapping()
     public List<Bike> list() {
-        List<Bike> bikes = new ArrayList<>();
-        return bikes;
+        return bikeRepository.findAll();
     }
 
     // Create bike
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void create(@RequestBody Bike bike) {
-
+        bikeRepository.save(bike);
     }
 
     // View specific bike based on id.
     @GetMapping("/{id}")
     public Bike get(@PathVariable("id") long id) {
-        return new Bike();
+        return bikeRepository.getOne(id);
     }
 
 }
